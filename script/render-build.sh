d89f6bc77ee6e68390f972bd3ffbd29ff8c67937
t0nylombardi <alombardi.331@gmail.com>
2023-12-12 17:34:19 -0500

    edit render-build 2

--- script/render-build.sh
+++ script/render-build.sh
@@ -5,5 +5,5 @@ npm rebuild esbuild && yarn
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:drop db:create
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create
 bundle exec rails db:migrate
