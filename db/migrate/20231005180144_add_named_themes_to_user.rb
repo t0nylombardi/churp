f43ba9cb163de1f184b0c2d1178d9df92480cbc2
t0nylombardi <alombardi.331@gmail.com>
2023-12-12 17:40:54 -0500

    edit migration again

--- db/migrate/20231005180144_add_named_themes_to_user.rb
+++ db/migrate/20231005180144_add_named_themes_to_user.rb
@@ -7,7 +7,7 @@ class AddNamedThemesToUser < ActiveRecord::Migration[7.0]
       t.string :display_name, null: true
       t.string :slug, null: true
       t.integer :theme, :integer, default: 0
-      t.integer :users, :theme_color, :integer, default: 0
+      t.integer :theme_color, :integer, default: 0
     end
 
     add_index :users, :username
