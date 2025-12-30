b2352f5518cd595fd0016d959a70937a9cc1f4ad
t0nylombardi <alombardi.331@gmail.com>
2025-12-30 18:28:51 -0500

    feat: Remove Devise authentication from User model
    Eliminate Devise-related fields and indexes from the User model and associated files to simplify user management. This change prepares the application for a new authentication strategy.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_19_210244) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_30_195709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -204,37 +204,16 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_19_210244) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "confirmation_sent_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
     t.datetime "created_at", null: false
-    t.datetime "current_sign_in_at"
-    t.string "current_sign_in_ip"
     t.string "display_name"
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "jti", null: false
-    t.datetime "last_sign_in_at"
-    t.string "last_sign_in_ip"
-    t.datetime "locked_at"
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
-    t.string "reset_password_token"
     t.integer "role"
-    t.integer "sign_in_count", default: 0, null: false
     t.string "slug"
-    t.string "unconfirmed_email"
-    t.string "unlock_token"
     t.datetime "updated_at", null: false
     t.string "username"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["display_name"], name: "index_users_on_display_name"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["jti"], name: "index_users_on_jti", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
