53abc19b02b670a39f004fb3267cecf2347b4716
Anthony Lombardi <alombardi.331@gmail.com>
2026-01-10 18:43:21 -0500

    feat: Simplify auth token generation in AuthHelper

--- spec/support/helpers/auth_helper.rb
+++ spec/support/helpers/auth_helper.rb
@@ -2,19 +2,7 @@
 
 module AuthHelper
   def auth_token_for(user, password:)
-    post "/api/v1/authentication/login",
-      params: {
-        email: user.email,
-        password:
-      }.to_json,
-      headers: json_headers
-    expect(response).to have_http_status(:ok)
-
-    parsed = JSON.parse(response.body)
-    token = parsed.dig("token", "value")
-    raise "Auth token missing: #{parsed}" unless token
-
-    token
+    Authentication::Tokens::JwtEncoder.encode({ user_id: user.id })
   end
 
   def auth_headers_for(user, password:)
