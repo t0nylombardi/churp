a7a428e603740e6db9725971a84591ba5f0fe8dc
Anthony Lombardi <alombardi.331@gmail.com>
2026-01-13 19:46:31 -0500

    chore: Update schema information in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :uuid             not null, primary key
+#  content    :jsonb            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  churp_id   :uuid             not null
+#  user_id    :uuid             not null
+#
+# Indexes
+#
+#  index_comments_on_churp_id  (churp_id)
+#  index_comments_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (churp_id => churps.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Comment < ApplicationRecord
   belongs_to :churp
   belongs_to :user
